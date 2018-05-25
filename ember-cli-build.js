f81a5ac99da11b884bc11700a33b06bac246e913
Lucas Recoaro <lucas.recoaro@realestate.co.nz>
2018-05-25 15:56:44 +1200

    First commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,8 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+
+  app.import('vendor/webtorrent.min.js');
+
   return app.toTree();
 };
